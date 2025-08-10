8e96d1818514296b6da6b8abd6f8f0b8cfd34ace
puffo <puffo@users.noreply.github.com>
2025-08-10 18:17:28 +0200

    Bump packages

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,16 +1,16 @@
 # Pin npm packages for Editor.js using esm.sh CDN for reliable ESM support
-pin "@editorjs/editorjs", to: "https://esm.sh/@editorjs/editorjs@2.27.2"
-pin "@editorjs/header", to: "https://esm.sh/@editorjs/header@2.7.0"
-pin "@editorjs/delimiter", to: "https://esm.sh/@editorjs/delimiter@1.3.0"
-pin "@editorjs/nested-list", to: "https://esm.sh/@editorjs/nested-list@1.3.0"
-pin "@editorjs/quote", to: "https://esm.sh/@editorjs/quote@2.5.0"
-pin "@editorjs/image", to: "https://esm.sh/@editorjs/image@2.8.1"
+pin "@editorjs/editorjs", to: "https://esm.sh/@editorjs/editorjs@2.30.8"
+pin "@editorjs/header", to: "https://esm.sh/@editorjs/header@2.8.8"
+pin "@editorjs/delimiter", to: "https://esm.sh/@editorjs/delimiter@1.4.2"
+pin "@editorjs/nested-list", to: "https://esm.sh/@editorjs/nested-list@1.4.3"
+pin "@editorjs/quote", to: "https://esm.sh/@editorjs/quote@2.7.6"
+pin "@editorjs/image", to: "https://esm.sh/@editorjs/image@2.10.3"
 pin "@editorjs/raw", to: "https://esm.sh/@editorjs/raw@2.5.1"
 
 # Inline tools
-pin "@editorjs/marker", to: "https://esm.sh/@editorjs/marker@1.3.0"
-pin "@editorjs/inline-code", to: "https://esm.sh/@editorjs/inline-code@1.4.0"
-pin "@editorjs/underline", to: "https://esm.sh/@editorjs/underline@1.1.0"
+pin "@editorjs/marker", to: "https://esm.sh/@editorjs/marker@1.4.0"
+pin "@editorjs/inline-code", to: "https://esm.sh/@editorjs/inline-code@1.5.2"
+pin "@editorjs/underline", to: "https://esm.sh/@editorjs/underline@1.2.1"
 pin "@sotaproject/strikethrough", to: "https://esm.sh/@sotaproject/strikethrough@1.0.1"
 
 # Pin the main editorjs-rails module
